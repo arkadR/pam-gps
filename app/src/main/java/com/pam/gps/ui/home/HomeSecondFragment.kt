a0f86d9b7845b9492690ed47d4317effcbb5b805
Michał Engel <michallengel@gmail.com>
2019-11-04 13:28:06 +0100

    refactored to global code style

--- app/src/main/java/com/pam/gps/ui/home/HomeSecondFragment.kt
+++ app/src/main/java/com/pam/gps/ui/home/HomeSecondFragment.kt
@@ -15,24 +15,24 @@ import com.pam.gps.R
 
 class HomeSecondFragment : Fragment() {
 
-    private val args: HomeSecondFragmentArgs by navArgs()
+  private val args: HomeSecondFragmentArgs by navArgs()
 
-    override fun onCreateView(
-            inflater: LayoutInflater, container: ViewGroup?,
-            savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home_second, container, false)
+  override fun onCreateView(
+    inflater: LayoutInflater, container: ViewGroup?,
+    savedInstanceState: Bundle?
+  ): View? {
+    // Inflate the layout for this fragment
+    return inflater.inflate(R.layout.fragment_home_second, container, false)
+  }
+
+  override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+    super.onViewCreated(view, savedInstanceState)
+
+    view.findViewById<TextView>(R.id.textview_home_second).text =
+      getString(R.string.hello_home_second, args.myArg)
+
+    view.findViewById<Button>(R.id.button_home_second).setOnClickListener {
+      findNavController().navigate(R.id.action_HomeSecondFragment_to_HomeFragment)
     }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-
-        view.findViewById<TextView>(R.id.textview_home_second).text =
-                getString(R.string.hello_home_second, args.myArg)
-
-        view.findViewById<Button>(R.id.button_home_second).setOnClickListener {
-            findNavController().navigate(R.id.action_HomeSecondFragment_to_HomeFragment)
-        }
-    }
+  }
 }
