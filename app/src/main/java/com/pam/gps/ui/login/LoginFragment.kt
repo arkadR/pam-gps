3391cd183f73bac7844c3d72c9c108838ab3be76
Michał Engel <michallengel@gmail.com>
2019-11-13 16:44:48 +0100

    Working login validation

--- app/src/main/java/com/pam/gps/ui/login/LoginFragment.kt
+++ app/src/main/java/com/pam/gps/ui/login/LoginFragment.kt
@@ -8,7 +8,13 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException
+import com.google.firebase.auth.FirebaseAuthInvalidUserException
 import com.pam.gps.R
+import com.pam.gps.extensions.validate
+import com.pam.gps.extensions.validateEmail
+import com.pam.gps.extensions.withValidateEmail
+import com.pam.gps.utils.hideKeyboard
 import kotlinx.android.synthetic.main.fragment_login.*
 import kotlinx.coroutines.CoroutineExceptionHandler
 import timber.log.Timber
@@ -26,30 +32,46 @@ class LoginFragment : Fragment() {
 
   override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
     super.onViewCreated(view, savedInstanceState)
-
     viewModel.authStatus.observe(
       viewLifecycleOwner, Observer {
         findNavController().navigate(R.id.action_navigation_login_to_navigation_home)
-      }
-    )
-
-  }
-
-
-  override fun onActivityCreated(savedInstanceState: Bundle?) {
-    super.onActivityCreated(savedInstanceState)
-    //TODO Error handling
-    val handler = CoroutineExceptionHandler { _, throwable ->
-      when (throwable) {
-        is Exception -> Timber.e(throwable)
-      }
-    }
+      })
     login_button.setOnClickListener {
+      if (!username_text.validateEmail()) {
+        login_layout.error = resources.getString(R.string.email_error)
+        username_text.requestFocus()
+        return@setOnClickListener
+      }
+      login_layout.error = null
+      if (!password_text.validate(String::isNotEmpty)) {
+        password_layout.error = resources.getString(R.string.password_error_empty)
+        password_text.requestFocus()
+        return@setOnClickListener
+      }
+      password_layout.error = null
+
+      hideKeyboard(requireContext(), view)
+      val handler = CoroutineExceptionHandler { _, throwable ->
+        when (throwable) {
+          is FirebaseAuthInvalidUserException -> {
+            Timber.e(throwable)
+          }
+          is FirebaseAuthInvalidCredentialsException -> {
+            password_layout.error = resources.getString(R.string.password_error_match)
+            Timber.e(throwable)
+          }
+          else -> Timber.e(throwable)
+        }
+      }
       viewModel.authenticate(
         username_text.text.toString(),
         password_text.text.toString(),
         handler
       )
     }
+    username_text.withValidateEmail(
+      login_layout,
+      resources.getString(R.string.email_error)
+    )
   }
 }
---
+++ app/src/main/java/com/pam/gps/utils/Utils.kt
@@ -0,0 +1,11 @@
+package com.pam.gps.utils
+
+import android.content.Context
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+
+
+fun hideKeyboard(context: Context, view: View): Unit {
+  val imm = context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+  imm.hideSoftInputFromWindow(view.windowToken, 0)
+}
