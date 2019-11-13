125c242a3463f076febddf0ed87fac940e91a9e1
Arkadiusz Rasz <arkadiusz.rasz@gmail.com>
2019-11-13 16:46:08 +0100

    Working map, gradle cleanup

--- app/src/main/java/com/pam/gps/ui/map/MapFragment.kt
+++ app/src/main/java/com/pam/gps/ui/map/MapFragment.kt
@@ -6,10 +6,17 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.viewModels
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 
 import com.pam.gps.R
+import kotlinx.android.synthetic.main.fragment_map.*
+import kotlinx.android.synthetic.main.fragment_map.view.*
 
-class MapFragment : Fragment() {
+class MapFragment : Fragment(), OnMapReadyCallback {
 
   companion object {
     fun newInstance() = MapFragment()
@@ -21,11 +28,20 @@ class MapFragment : Fragment() {
     inflater: LayoutInflater, container: ViewGroup?,
     savedInstanceState: Bundle?
   ): View? {
-    return inflater.inflate(R.layout.fragment_map, container, false)
+    var view = inflater.inflate(R.layout.fragment_map, container, false)
+    view.mapView.onCreate(savedInstanceState)
+    view.mapView.getMapAsync(this);
+    return view;
   }
 
   override fun onActivityCreated(savedInstanceState: Bundle?) {
     super.onActivityCreated(savedInstanceState)
+  }
+
+
+  override fun onMapReady(googleMap: GoogleMap) {
+    googleMap.isMyLocationEnabled = true
+    mapView.onResume()
   }
 
 }
